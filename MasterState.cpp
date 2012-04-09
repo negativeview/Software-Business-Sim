514b078599a5ab0ca4c0921b50d4983087b31d04
Daniel Grace <negativeview@gmail.com>
2012-04-09 17:40:31 -0500

    Add a lot more names to the random generator.

--- MasterState.cpp
+++ MasterState.cpp
@@ -92,57 +92,114 @@ list <string> *MasterState::advanceTime(int amount) {
 
 void MasterState::_setupLanguages() {
 	this->_allLanguages = new vector<Language *>();
-	this->_allLanguages->push_back(new Language("PHP"));
 	this->_allLanguages->push_back(new Language("C"));
 	this->_allLanguages->push_back(new Language("C++"));
-	this->_allLanguages->push_back(new Language("Python"));
 	this->_allLanguages->push_back(new Language("JavaScript"));
+	this->_allLanguages->push_back(new Language("Lua"));
+	this->_allLanguages->push_back(new Language("Perl"));
+	this->_allLanguages->push_back(new Language("PHP"));
+	this->_allLanguages->push_back(new Language("Python"));
 	this->_allLanguages->push_back(new Language("Ruby"));
 }
 
 void MasterState::_setupPlatforms() {
 	this->_allPlatforms = new vector<Platform *>();
-	this->_allPlatforms->push_back(new Platform("desktop"));
-	this->_allPlatforms->push_back(new Platform("browser"));
-	this->_allPlatforms->push_back(new Platform("console"));
-	this->_allPlatforms->push_back(new Platform("mobile"));
-	this->_allPlatforms->push_back(new Platform("server"));
+	this->_allPlatforms->push_back(new Platform("Browser"));
+	this->_allPlatforms->push_back(new Platform("Console"));
+	this->_allPlatforms->push_back(new Platform("Desktop"));
+	this->_allPlatforms->push_back(new Platform("Mobile"));
+	this->_allPlatforms->push_back(new Platform("Server"));
 }
 
 void MasterState::_setupLastNames() {
 	this->_lastNames = new vector<string>();
+	this->_lastNames->push_back("Amundson");
 	this->_lastNames->push_back("Anderson");
 	this->_lastNames->push_back("Anniston");
+	this->_lastNames->push_back("Birthler");
+	this->_lastNames->push_back("Bremer");
+	this->_lastNames->push_back("Cannon");
+	this->_lastNames->push_back("Compton");
+	this->_lastNames->push_back("Dorland");
+	this->_lastNames->push_back("Esbensen");
+	this->_lastNames->push_back("Frazier");
+	this->_lastNames->push_back("Gemoets");
+	this->_lastNames->push_back("Gloege");
 	this->_lastNames->push_back("Gohde");
 	this->_lastNames->push_back("Grace");
+	this->_lastNames->push_back("Green");
+	this->_lastNames->push_back("Hemmingway");
+	this->_lastNames->push_back("Jones");
+	this->_lastNames->push_back("Kamke");
+	this->_lastNames->push_back("Koehl");
+	this->_lastNames->push_back("Lamb");
+	this->_lastNames->push_back("Mars");
+	this->_lastNames->push_back("Mason");
 	this->_lastNames->push_back("Miller");
+	this->_lastNames->push_back("Myers");
+	this->_lastNames->push_back("Olson");
 	this->_lastNames->push_back("Pepper");
+	this->_lastNames->push_back("Poeschl");
+	this->_lastNames->push_back("Reed");
+	this->_lastNames->push_back("Reynolds");
+	this->_lastNames->push_back("Runyon");
+	this->_lastNames->push_back("Schilken");
+	this->_lastNames->push_back("Schneider");
 	this->_lastNames->push_back("Seemann");
+	this->_lastNames->push_back("Simula");
+	this->_lastNames->push_back("Smith");
+	this->_lastNames->push_back("Spradlin");
+	this->_lastNames->push_back("Tava");
+	this->_lastNames->push_back("Wade");
 	this->_lastNames->push_back("Wolever");
+	this->_lastNames->push_back("Wurm");
 }
 
 void MasterState::_setupFirstNames() {
 	this->_firstNames = new vector<string>();
+	this->_firstNames->push_back("Aaron");
+	this->_firstNames->push_back("Albert");
 	this->_firstNames->push_back("Alice");
+	this->_firstNames->push_back("Amanda");
+	this->_firstNames->push_back("Antoinette");
 	this->_firstNames->push_back("Barbara");
+	this->_firstNames->push_back("Ben");
+	this->_firstNames->push_back("Brad");
+	this->_firstNames->push_back("Breen");
 	this->_firstNames->push_back("Brianna");
 	this->_firstNames->push_back("Charles");
+	this->_firstNames->push_back("Chelsea");
 	this->_firstNames->push_back("Christopher");
+	this->_firstNames->push_back("Dan");
 	this->_firstNames->push_back("Daniel");
+	this->_firstNames->push_back("Dave");
 	this->_firstNames->push_back("David");
+	this->_firstNames->push_back("Devin");
 	this->_firstNames->push_back("Donald");
 	this->_firstNames->push_back("Elizabeth");
+	this->_firstNames->push_back("Erica");
+	this->_firstNames->push_back("Eve");
+	this->_firstNames->push_back("Gary");
+	this->_firstNames->push_back("Gillian");
+	this->_firstNames->push_back("Gina");
+	this->_firstNames->push_back("Greg");
+	this->_firstNames->push_back("Hannah");
 	this->_firstNames->push_back("James");
+	this->_firstNames->push_back("Jamie");
+	this->_firstNames->push_back("Jenna");
 	this->_firstNames->push_back("Jennifer");
 	this->_firstNames->push_back("Jeremy");
 	this->_firstNames->push_back("John");
 	this->_firstNames->push_back("Joseph");
 	this->_firstNames->push_back("Julia");
 	this->_firstNames->push_back("Linda");
+	this->_firstNames->push_back("Lisa");
 	this->_firstNames->push_back("Maria");
 	this->_firstNames->push_back("Markus");
 	this->_firstNames->push_back("Mark");
 	this->_firstNames->push_back("Mary");
+	this->_firstNames->push_back("Matt");
+	this->_firstNames->push_back("Matthew");
 	this->_firstNames->push_back("Michael");
 	this->_firstNames->push_back("Patricia");
 	this->_firstNames->push_back("Paul");
