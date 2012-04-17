3964670b67c99e4d6c5b5e0219d4a1398ee8a13d
Daniel Grace <negativeview@gmail.com>
2012-04-17 17:02:36 -0500

    Added a better employee view with details in the sidebar.

--- EmployeesCommand.cpp
+++ EmployeesCommand.cpp
@@ -2,6 +2,7 @@
 #include "EmployeesCommand.h"
 #include "MasterState.h"
 #include "Person.h"
+#include "Trait.h"
 
 EmployeesCommand::EmployeesCommand(MasterState *masterState) : CommandFunctor(masterState, "employees", "Hire an employee to work in your business.") {
 	
@@ -13,6 +14,13 @@ void EmployeesCommand::executeCommand(const char *line) {
 	MENU *my_menu = this->setupMenu(this->_masterState->getPlayerCompany()->getEmployees());
 	WINDOW *my_menu_win = this->setupWindow(my_menu, "Employees");
 	refresh();
+
+	WINDOW *my_details_win = newwin(10, 30, 4, 44);
+    box(my_details_win, 0, 0);
+    mvwprintw(my_details_win, 1, 2, "Details");
+	mvwaddch(my_details_win, 2, 0, ACS_LTEE);
+	mvwhline(my_details_win, 2, 1, ACS_HLINE, 38);
+	mvwaddch(my_details_win, 2, 39, ACS_RTEE);
 
     int c;
     int done = 0;
@@ -36,7 +44,25 @@ void EmployeesCommand::executeCommand(const char *line) {
 				done = 1;
 				break;
 		}
+
 		wrefresh(my_menu_win);
+
+		ITEM *currentItem = current_item(my_menu);
+		if (currentItem) {
+			Person *p = (Person *)item_userptr(currentItem);
+
+			list<Trait *> *realTraits = p->getRealTraits();
+			int down = 3;
+			for(list<Trait *>::iterator it = realTraits->begin(); it != realTraits->end(); ++it) {
+				Trait *t = *it;
+				char message[30];
+				sprintf(message, "%10s: %s", t->getName().c_str(), t->getDiscoveryModifiedText().c_str());
+				wmove(my_details_win, down, 2);
+				down++;
+				wprintw(my_details_win, message);
+			}
+		}
+		wrefresh(my_details_win);
 	}
 
 	/* Unpost and free all the memory taken up */
